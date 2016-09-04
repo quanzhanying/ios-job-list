9dd629a1e7c794c7782a29f8268628f27dcd9f25
xdite <xuite.joke@gmail.com>
2016-09-04 21:04:46 +0800

    job content

--- app/screens/job_screen.rb
+++ app/screens/job_screen.rb
@@ -5,6 +5,11 @@ class JobScreen < PM::Screen
 
   def on_load
     self.title = @job.title
+
+    @content = append!(UILabel, :job_content)
+    @content.text = @job.content
+
+    @image = append!(UIImageView, :job_image).style { |st| st.remote_image = @job.image_url }
   end
 
   def will_animate_rotate(_orientation, _duration)
--- app/stylesheets/job_screen_stylesheet.rb
+++ app/stylesheets/job_screen_stylesheet.rb
@@ -7,6 +7,16 @@ class JobScreenStylesheet < ApplicationStylesheet
     # Add application specific setup stuff in application_stylesheet.rb
   end
 
+  def job_content(st)
+    st.frame = { top: 100, left: 20, width: 100, height: 30 }
+    st.color = color.black
+  end
+
+  def job_image(st)
+    st.frame = { top: 200, left: 10, width: 200, height: 200 }
+    st.background_color = color.black
+  end
+
   def root_view(st)
     st.background_color = color.white
   end
